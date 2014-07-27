2fcc4d57c1831b3e9b85f2fc8f1bd98be8ac8899
briardes <brian@Brians-MacBook-Air.local>
2014-07-27 13:37:13 -0400

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(firstname: 'Samuel', lastname: 'Jackson', bio: '
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+  arcu eget nisi posuere feugiat non vitae ligula. Morbi ut purus eget
+  diam hendrerit scelerisque. Aliquam fermentum neque in suscipit faucibus.
+  Maecenas feugiat orci non velit sodales, vitae dapibus purus vulputate.
+  Duis semper, ligula non gravida convallis, ante tortor interdum massa, in
+  ultricies mauris lacus et velit.', github_username: 'Sjacks', twitter_url: 'https://twitter.com/',
+  linkedin_url: 'https://www.linkedin.com/',cohort_class: 'Front End')
+Student.create(firstname: 'Jacob', lastname: 'Lillis', bio: '
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+  arcu eget nisi posuere feugiat non vitae ligula. Morbi ut purus eget
+  diam hendrerit scelerisque. Aliquam fermentum neque in suscipit faucibus.
+  Maecenas feugiat orci non velit sodales, vitae dapibus purus vulputate.
+  Duis semper, ligula non gravida convallis, ante tortor interdum massa, in
+  ultricies mauris lacus et velit.', github_username: 'Jlillis', twitter_url: 'https://twitter.com/',
+               linkedin_url: 'https://www.linkedin.com/',cohort_class: 'Back End')
+Student.create(firstname: 'Chris', lastname: 'Anderson', bio: '
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+  arcu eget nisi posuere feugiat non vitae ligula. Morbi ut purus eget
+  diam hendrerit scelerisque. Aliquam fermentum neque in suscipit faucibus.
+  Maecenas feugiat orci non velit sodales, vitae dapibus purus vulputate.
+  Duis semper, ligula non gravida convallis, ante tortor interdum massa, in
+  ultricies mauris lacus et velit.', github_username: 'Canderson', twitter_url: 'https://twitter.com/',
+               linkedin_url: 'https://www.linkedin.com/',cohort_class: 'Back End')
