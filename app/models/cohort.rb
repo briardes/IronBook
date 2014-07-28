6bcaffae234463785e51ca1815a43600cfbe79c6
Ichus <Ichus.local>
2014-07-28 15:41:27 -0400

    Added Gems for testing and Model Tests.

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,5 @@
 class Cohort < ActiveRecord::Base
-  has_many :students
+  has_many :students, dependent: :destroy
+
+  validates :start_date, :end_date, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
   belongs_to :cohort
   belongs_to :user
+
+  validates :firstname, :lastname, :cohort_class, presence: true
 end
